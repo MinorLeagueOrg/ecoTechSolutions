c656a1b4b00b05635011d9681df14c7557bbda24
Diomedes Trinidad <diomedesinf39@hotmail.com>
2025-10-02 01:07:25 -0300

    Base de datos MySQL
    Se agregaron correcciones de AUTOINCREMENT a la base de datos, también nuevas tablas y relaciones entre las mismas. Se inserte el procedure InsertarPorDefecto, que busca crear datos en las tablas independientes para luego partir el programa desde ahí.

---
+++ test.sql
@@ -0,0 +1,431 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-10-2025 a las 06:02:56
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `test`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertarDatosPorDefecto` ()   BEGIN 
+	INSERT INTO usuarios(usuario, contrasena) VALUES ('superadmin', '123456');
+    INSERT INTO rol(nombre, descripcion) VALUES ('superadmin', 'Este es el rol master del sistema');
+    INSERT INTO rol(nombre, descripcion) VALUES ('admin', 'Este rol le pertenece a la persona con mas cargo de la empresa');
+    INSERT INTO rol(nombre, descripcion) VALUES ('usuario', 'Este es el rol de los empleados con mas restricciones de la empresa');
+    INSERT INTO proyectos(nombre, descripcion) VALUES ('ProjectOne', 'Este es el primer proyecto de entrenamiento para los empleados nuevos');
+    INSERT INTO departamentos(nombre, descripcion) VALUES ('DeptOne', 'Este es el primer departamento donde entrenan a los empleados nuevos');
+    -- TODO: AGREGAR LOS INSERT DE LOS MODULOS
+    -- TODO: AGREGAR UN TRIGGER PARA QUE CUANDO SE INGRESE UN EMPLEADO DEPENDIENDO DE SU ROL TENGA LOS PERMISOS LISTOS
+    -- TODO: AGREGAR LOS MODULOS
+    SELECT 'Datos por defecto insertados correctamente.' AS Resultado;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignacion_gerente`
+--
+
+CREATE TABLE `asignacion_gerente` (
+  `id` int(11) NOT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  `id_empleado` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `asignacion_gerente`
+--
+
+INSERT INTO `asignacion_gerente` (`id`, `id_departamento`, `id_empleado`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignacion_proyectos`
+--
+
+CREATE TABLE `asignacion_proyectos` (
+  `id` int(11) NOT NULL,
+  `id_empleado` int(11) DEFAULT NULL,
+  `id_proyecto` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `id_depto` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `gerente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamentos`
+--
+
+INSERT INTO `departamentos` (`id_depto`, `nombre`, `descripcion`, `gerente`) VALUES
+(1, 'Ventas', 'Departamento principal donde todos los empleados comienzan su labor', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id_empleado` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `fecha_de_inicio` date DEFAULT current_timestamp(),
+  `salario` decimal(10,2) DEFAULT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_departamento` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id_empleado`, `nombre`, `direccion`, `telefono`, `email`, `fecha_de_inicio`, `salario`, `id_rol`, `id_departamento`, `id_usuario`) VALUES
+(1, 'Diomedes Trinidad', 'Lautaro Navarro 0362', '56984731830', 'diomedesinf39@hotmail.com', '2025-10-01', 700000.00, 3, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos`
+--
+
+CREATE TABLE `modulos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Registrar Usuario', 'Gestione los usuarios'),
+(2, 'Generar informe', 'Genera informe de tu trabajo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_modulo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id`, `id_rol`, `id_modulo`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 3, 1),
+(4, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proyectos`
+--
+
+INSERT INTO `proyectos` (`id`, `nombre`, `descripcion`, `fecha_inicio`) VALUES
+(1, 'Project One', 'Este es el proyecto de entrenamiento para todas las personas que ingresan a la empresa', '2025-10-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_tiempos`
+--
+
+CREATE TABLE `registro_tiempos` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `cantidad_de_horas` decimal(5,2) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_proyecto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'superadmin', 'Este es el usuario inicial y principal del sistema y es quien tiene el primer usuario'),
+(2, 'admin', 'Este es el rol de administrador y es quien tiene la mayor cantidad de permisos'),
+(3, 'usuario_general', 'Este es el rol para los usuarios que utilicen el sistema en general');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `contrasena` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `contrasena`) VALUES
+(1, 'dtrinidad', '$2b$12$1LM0hpgcaYgfFIWqny963ugXOfVro8gOVVS.M3tqUEhncu6OSfMeK'),
+(2, 'cmartinez', '$2b$12$eHafwUT2Lw83bOouoWen3uipPpor4XmhHEuozFdOMHQfR8nTmmjr.');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignacion_gerente`
+--
+ALTER TABLE `asignacion_gerente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_asigdept` (`id_departamento`),
+  ADD KEY `fk_asigemp` (`id_empleado`);
+
+--
+-- Indices de la tabla `asignacion_proyectos`
+--
+ALTER TABLE `asignacion_proyectos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_asigpro_empl` (`id_empleado`),
+  ADD KEY `fk_asigempl_pro` (`id_proyecto`);
+
+--
+-- Indices de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD PRIMARY KEY (`id_depto`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_departamento` (`id_departamento`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `modulos`
+--
+ALTER TABLE `modulos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_modulo` (`id_modulo`);
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `registro_tiempos`
+--
+ALTER TABLE `registro_tiempos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_proyecto` (`id_proyecto`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignacion_gerente`
+--
+ALTER TABLE `asignacion_gerente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `asignacion_proyectos`
+--
+ALTER TABLE `asignacion_proyectos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  MODIFY `id_depto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `modulos`
+--
+ALTER TABLE `modulos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_tiempos`
+--
+ALTER TABLE `registro_tiempos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `asignacion_gerente`
+--
+ALTER TABLE `asignacion_gerente`
+  ADD CONSTRAINT `fk_asigdept` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_depto`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_asigemp` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `asignacion_proyectos`
+--
+ALTER TABLE `asignacion_proyectos`
+  ADD CONSTRAINT `fk_asigempl_pro` FOREIGN KEY (`id_proyecto`) REFERENCES `proyectos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_asigpro_empl` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD CONSTRAINT `departamentos_ibfk_1` FOREIGN KEY (`gerente`) REFERENCES `empleados` (`id_empleado`);
+
+--
+-- Filtros para la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`),
+  ADD CONSTRAINT `empleados_ibfk_2` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_depto`),
+  ADD CONSTRAINT `empleados_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`),
+  ADD CONSTRAINT `permisos_ibfk_2` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`);
+
+--
+-- Filtros para la tabla `registro_tiempos`
+--
+ALTER TABLE `registro_tiempos`
+  ADD CONSTRAINT `registro_tiempos_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
+  ADD CONSTRAINT `registro_tiempos_ibfk_2` FOREIGN KEY (`id_proyecto`) REFERENCES `proyectos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
